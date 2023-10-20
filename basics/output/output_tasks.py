f1e8fefd93764ab84ab4b574f6b08ed0049ecbff
p-jab <1zapor33@solent.ac.uk>
2023-10-20 13:16:50 +0100

    Demo and solution to  1task

---
+++ basics/output/output_tasks.py
@@ -0,0 +1,9 @@
+#display message on screen
+print("welcome to QHO 426!")
+print(''' In week one we are going to learn :
+
+             ...Git and Github
+             ...how to output to the screen
+             ...How to het user input
+            
+            I hope you are enjoying the lesson right now''')
---
+++ weeek1.py
@@ -0,0 +1,4 @@
+print("hello world")
+name = input("what is your name?\n")
+age = int(input("what is your age?\n"))
+print(f"welcome to QHO426, {name} of age {age}")
