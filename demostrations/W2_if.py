2532181c31755f51e6573a0e870cc7a9888e1971
p-jab <1zapor33@solent.ac.uk>
2023-10-25 22:29:27 +0100

    add two demos

---
+++ demostrations/W2_if.py
@@ -0,0 +1,11 @@
+name = input ("enter your name:" )
+
+if len(name) > 5:
+    print("you have a long name")
+elif len(name) < 3:
+    print("your name is super short")
+elif name.lower() == "piotr":
+    print("that's the best name ever")
+else:
+    print("what is your name?")
+ print("welcome to phyton")
